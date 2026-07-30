6b3ea9e1667c8d4558ddf3651176705345dce509
Georgiy <georgcenko@gmail.com>
2026-07-30 12:19:37 +0300

    initial

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function getCoordinates(city) {
-    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`)
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=ru`)
     const data = await response.json()
     return data.results[0]
 }
@@ -12,11 +12,26 @@ const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=$
     return data
 }
 
-async function main() {
-    let city = document.querySelector('#CitySelect').value
-    let place = await getCoordinates(city)
-    let weather = await getWeather(place.latitude, place.longtude)
-    console.log(weather) 
+function renderCurrent (weather) {
+const current = weather.current;
+
+document.querySelector('#currentWeather').innerHTML =`
+<h2>Поточна погода</h2>
+<p> 🌡${current.temperature_2m}°C</p>
+<p> 🤗Видчуваєтся: ${current.apparent_temperature}°C</p>
+<p> 💧Вологисть: ${current.relative_humidity_2m}%</p>
+<p> 🌫Вітер: ${current.wind_speed_10m} км/год</p>
+`;
 }
 
-main()
+async function main(city) {
+    let place = await getCoordinates(city)
+    let weather = await getWeather(place.latitude, place.longitude)
+    console.log(weather)
+    renderCurrent(weather) 
+}
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+    const city = document.getElementById("cityInput").value;
+    main(city);
+})
