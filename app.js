fe7e1a78c63a97a228c5c1fbaaa6f37040862e43
Georgiy <georgcenko@gmail.com>
2026-08-13 17:51:41 +0300

    add enter listener

--- app.js
+++ app.js
@@ -15,29 +15,28 @@ async function getCoordinates(city) {
 }
 
 async function getWeather(latitude, longitude) {
-const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}
+    const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}
 &current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,weather_code
 &daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`)
 
     if (!response.ok) {
         throw new Error('Bad response ${response.status}');
-}
+    }
 
     const data = await response.json()
-    
-   return data
+    return data
 }
 
 function renderCurrent (weather) {
-const current = weather.current;
+    const current = weather.current;
 
-document.querySelector('#currentWeather').innerHTML =`
-<h2>Поточна погода</h2>
-<p> 🌡${current.temperature_2m}°C</p>
-<p> 🤗Видчуваєтся: ${current.apparent_temperature}°C</p>
-<p> 💧Вологисть: ${current.relative_humidity_2m}%</p>
-<p> 🌫Вітер: ${current.wind_speed_10m} км/год</p>
-`;
+    document.querySelector('#currentWeather').innerHTML =`
+    <h2>Поточна погода</h2>
+    <p> 🌡${current.temperature_2m}°C</p>
+    <p> 🤗Видчуваєтся: ${current.apparent_temperature}°C</p>
+    <p> 💧Вологисть: ${current.relative_humidity_2m}%</p>
+    <p> 🌫Вітер: ${current.wind_speed_10m} км/год</p>
+    `;
 }
 
 async function main(city) {
@@ -55,4 +54,11 @@ async function main(city) {
 document.getElementById("searchBtn").addEventListener("click", () => {
     const city = document.getElementById("cityInput").value;
     main(city);
+})
+
+document.getElementById("cityInput").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        const city = document.getElementById("cityInput").value;
+        main(city); 
+    }
 })
